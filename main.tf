2791c222a9298d23a0f50ee5140bd6b31e954ef4
Veekshitha04 <veekshithareddymuthadi@gmail.com>
2025-05-09 18:13:52 +0530

    second commit

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-data "azurerm_client_config" "current" {}
-
+data "azurerm_client_config" "current" {}   #to fetch info about tenanat,object id for keyvault accesss
+data "azuread_service_principal" "sp" {
+  client_id = "b8e0ea58-6ab1-4082-a770-0719bbcc6dba"
+}
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.prefix}-rg"
+  name     = var.res_grp_name
   location = var.location
 }
-
 resource "azurerm_key_vault" "kv" {
   name                        = var.keyvault_name
   location                    = azurerm_resource_group.rg.location
@@ -14,31 +15,35 @@ resource "azurerm_key_vault" "kv" {
   purge_protection_enabled    = false
   enable_rbac_authorization   = true
 }
-
-resource "azurerm_role_assignment" "kv_access" {
+resource "azurerm_role_assignment" "sp_kv_secrets_officer" {
   scope                = azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-resource "azurerm_role_assignment" "app_service_kv_access" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_app_service.as.identity[0].principal_id
+  principal_id         = data.azuread_service_principal.sp.object_id
 }
  
+resource "azurerm_role_assignment" "user_secret_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = "0478ae62-bf06-425a-904c-d23d941b3f1e"
+}
+resource "azurerm_key_vault_secret" "example1" {
+  name         = "SQL-PASSWORD1"
+  value        = var.sql_pa
+  key_vault_id = azurerm_key_vault.kv.id
+  depends_on          = [azurerm_role_assignment.sp_kv_secrets_officer]
+}
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = [var.vnet_address]
+  address_space       = var.v_add
 }
-
 resource "azurerm_subnet" "subnet1" {
   name                 = "${var.prefix}-s1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.web_subnet_prefix]
-
+  address_prefixes     = var.s1_add
+ 
   delegation {
     name = "delegation-to-web-serverfarms"
     service_delegation {
@@ -47,130 +52,122 @@ resource "azurerm_subnet" "subnet1" {
     }
   }
 }
-
-
 resource "azurerm_subnet" "subnet2" {
   name                 = "${var.prefix}-s2"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.sql_subnet_prefix]
+  address_prefixes     = var.s2_add
 }
-
 resource "azurerm_app_service_plan" "asp" {
-  name                = "${var.prefix}-asp"
+  name                = var.asn
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"
+  kind                = var.plan_kind
   reserved            = true
-
   sku {
-    tier = var.app_service_plan_tier
-    size = var.app_service_plan_sku
+    tier = var.plan_tier
+    size = var.plan_size
   }
 }
-
-resource "azurerm_app_service" "as" {
-  name                = "${var.prefix}-webapp"
+resource "azurerm_app_service" "apps" {
+  name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
-
+ 
+  site_config {
+    linux_fx_version = "NODE|18-lts"
+    always_on        = true
+  }
+ 
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    "SQL_USER"                 = "rajitha"
+    "SQL_SERVER"               = "rajitha-sql.database.windows.net"
+    "SQL_DATABASE"             = "rajitha-db"
+     "SQL_PASSWORD" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.example1.id})"
+ 
+   
+   
+   
+ 
+    "NODE_ENV" = "production"
+  }
   identity {
     type = "SystemAssigned"
   }
-
-  site_config {
-    linux_fx_version = "PYTHON|3.10"
-    always_on        = true
-  }
-
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "SQL_USER"     = var.sql_admin_username
-    "SQL_SERVER"   = "${azurerm_mssql_server.server.name}.database.windows.net"
-    "SQL_DATABASE" = "${var.prefix}-db"
-    "SQL_PASSWORD" = "StrongP@ssw0rd123"
-    "PYTHON_ENV"   = "production"
-  }
-
   lifecycle {
+ 
     ignore_changes = [
-      app_settings
+      source_control
     ]
+ 
+ 
   }
+  depends_on = [
+   azurerm_key_vault_secret.example1,
+  azurerm_role_assignment.sp_kv_secrets_officer,
+  azurerm_role_assignment.user_secret_user
+]
+ 
+ 
+ 
 }
-
+ 
+resource "azurerm_app_service_virtual_network_swift_connection" "connect" {
+  app_service_id = azurerm_app_service.apps.id
+  subnet_id      = azurerm_subnet.subnet1.id
+}
+ 
 resource "azurerm_mssql_server" "server" {
-  name                         = "${var.prefix}-sql"
+  name                         = var.sql_server_name
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
-  version                      = var.sql_server_version
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
-#   administrator_login          = data.azurerm_key_vault_secret.example1.value
-#   administrator_login_password = data.azurerm_key_vault_secret.example2.value
+  version                      = "12.0"
+  administrator_login          = var.server_login
+  administrator_login_password = var.server_pass
+ 
 }
-
-# resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
-#   name             = "AllowAzureServices"
-#   server_id        = azurerm_mssql_server.server.id
-#   start_ip_address = "0.0.0.0"
-#   end_ip_address   = "0.0.0.0"
-# }
-
-# resource "azurerm_mssql_database" "db" {
-#   name           = "${var.prefix}-db"
-#   server_id      = azurerm_mssql_server.server.id
-#   collation      = "SQL_Latin1_General_CP1_CI_AS"
-#   license_type   = "LicenseIncluded"
-#   max_size_gb    = 2
-#   sku_name       = var.sql_sku
-#   enclave_type   = "VBS"
-# }
-
-# resource "azurerm_private_endpoint" "conn" {
-#   name                = "${var.prefix}-conn"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   location            = azurerm_resource_group.rg.location
-#   subnet_id           = azurerm_subnet.subnet2.id
-
-#   private_service_connection {
-#     name                           = "${var.prefix}-sql-pe-connection"
-#     private_connection_resource_id = azurerm_mssql_server.server.id
-#     is_manual_connection           = false
-#     subresource_names              = ["sqlServer"]
-#   }
-# }
-
-# resource "azurerm_private_dns_zone" "sql_dns" {
-#   name                = "privatelink.database.windows.net"
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
-
-# resource "azurerm_private_dns_zone_virtual_network_link" "sql_dns_link" {
-#   name                  = "${var.prefix}-sql-dns-link"
-#   resource_group_name   = azurerm_resource_group.rg.name
-#   private_dns_zone_name = azurerm_private_dns_zone.sql_dns.name
-#   virtual_network_id    = azurerm_virtual_network.vnet.id
-# }
-
-# resource "azurerm_private_dns_a_record" "sql_a_record" {
-#   name                = azurerm_mssql_server.server.name
-#   zone_name           = azurerm_private_dns_zone.sql_dns.name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   ttl                 = 300
-#   records             = [azurerm_private_endpoint.conn.private_service_connection[0].private_ip_address]
-# }
-
-# resource "azurerm_app_service_virtual_network_swift_connection" "connect" {
-#   app_service_id = azurerm_app_service.as.id
-#   subnet_id      = azurerm_subnet.subnet1.id
-# }
-
-# output "app_service_name" {
-#   value = azurerm_app_service.as.name
-# }
-
-# output "resource_group_name" {
-#   value = azurerm_resource_group.rg.name
-# }
+ 
+resource "azurerm_mssql_database" "db" {
+  name         = "${var.prefix}-db"
+  server_id    = azurerm_mssql_server.server.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+}
+ 
+resource "azurerm_private_endpoint" "conn" {
+  name                = "${var.prefix}-conn"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = azurerm_subnet.subnet2.id
+ 
+  private_service_connection {
+    name                           = "example-sql-connection"
+    private_connection_resource_id = azurerm_mssql_server.server.id
+    is_manual_connection           = false
+    subresource_names              = ["sqlServer"]
+  }
+}
+ 
+resource "azurerm_private_dns_zone" "sql_dns" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+ 
+resource "azurerm_private_dns_zone_virtual_network_link" "sql_dns_link" {
+  name                  = "${var.prefix}-sdl"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.sql_dns.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+ 
+resource "azurerm_private_dns_a_record" "sql_a_record" {
+  name                = azurerm_mssql_server.server.name
+  zone_name           = azurerm_private_dns_zone.sql_dns.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.conn.private_service_connection[0].private_ip_address]
+}
--- variables.tf
+++ variables.tf
@@ -1,79 +1,49 @@
 variable "prefix" {
-  description = "Prefix for resource names"
-  type        = string
+ 
 }
-
 variable "location" {
-  description = "The Azure region where resources will be created"
-  type        = string
+ 
 }
-
-variable "keyvault_name"{
-    description="name of the keyvault"
-    type  = string
+variable "v_add" {
+ 
 }
-variable "vnet_address" {
-  description = "Address space for the virtual network"
-  type        = string
+variable "s1_add" {
+ 
 }
-
-variable "web_subnet_prefix" {
-  description = "Subnet address prefix for the web subnet"
-  type        = string
+variable "s2_add" {
+ 
 }
-
-variable "sql_subnet_prefix" {
-  description = "Subnet address prefix for the SQL subnet"
-  type        = string
+ 
+variable "plan_kind" {
+ 
 }
-
-variable "sql_database_name" {
-  description = "The name of the SQL database"
-  type        = string
+variable "plan_tier" {
+ 
 }
-
-variable "app_service_plan_tier" {
-  description = "The tier of the App Service Plan"
-  type        = string
+variable "plan_size" {
+ 
 }
-
-variable "app_service_plan_sku" {
-  description = "The SKU of the App Service Plan"
-  type        = string
+variable "server_login" {
+ 
 }
-
-variable "sql_admin_username" {
-  description = "The SQL server administrator username"
-  type        = string
+variable "server_pass" {
+ 
 }
-
-variable "sql_admin_password" {
-  description = "The SQL server administrator password"
-  type        = string
+variable "sql_pa" {
+ 
 }
-
-variable "sql_server_version" {
-  description = "The SQL server version"
-  type        = string
+variable "asn" {
+ 
 }
-
-variable "sql_sku" {
-  description = "The SKU of the SQL server"
-  type        = string
+variable "app_service_name" {
+ 
 }
-
-variable "web_app_name" {
-  description = "The name of the web application"
-  type        = string
+variable "sql_server_name" {
+ 
 }
-
-variable "service_connection" {
-  description = "The name of the private service connection"
-  type        = string
+variable "res_grp_name" {
+ 
 }
-
-variable "dns_name" {
-  description = "The DNS name for the private link"
-  type        = string
+variable "keyvault_name" {
+ 
 }
-
